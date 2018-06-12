7b8b9d8fc4b59d85ec9f7f7b5cf6636883d34e92
Arthur-Justino <ajmms@cin.ufpe.br>
2018-06-12 01:38:39 -0300

    Troca de pasta

---
+++ app/src/main/java/com/parse/starter/activities/DetalhesProdutoActivity.java
@@ -0,0 +1,25 @@
+package com.parse.starter.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.parse.starter.R;
+
+import org.w3c.dom.Text;
+
+public class DetalhesProdutoActivity extends AppCompatActivity {
+    private TextView nomeProd;
+    private TextView quantProd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_produto);
+        nomeProd = (TextView) findViewById(R.id.nomeProId);
+        quantProd = (TextView) findViewById(R.id.nomeQuantId);
+        Bundle extra = getIntent().getExtras();
+
+        nomeProd.setText(extra.getString("nomeProd"));
+        quantProd.setText(extra.getString ("quantProd"));
+    }
+}
